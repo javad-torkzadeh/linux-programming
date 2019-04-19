e702347799465847225b959e5b53578d09314e4b
javad torkzadeh <javadtorkzadeh1995@gmail.com>
2019-04-19 02:10:59 -0700

    sum

---
+++ sum.py
@@ -0,0 +1,13 @@
+dad1 = input("enter first float \n ")
+adad1 = float(adad1)
+adad2 = input ("enter secend float \n ")
+adad2 = float(adad2)
+adad3 = input("enter float three \n")
+adad3 = float(adad3)
+sum = adad1+adad2+adad3
+zarb = adad1*adad2*adad3
+avg = sum/3
+print  (sum)
+print (zarb)
+print (avg)
+
