929cd8595759d45f85e6b0ae17f67e6c1b8ea2c4
javad torkzadeh <javadtorkzadeh1995@gmail.com>
2019-04-21 02:39:01 -0700

    houre

--- test.py
+++ test.py
@@ -1,2 +1,10 @@
-print ("hello javad")
+integer = input("enter intiger")
+integer = int (integer)
+houre = integer//3600
+minite = (integer%3600)//60
+secend = (integer%3600)%60
+print (houre)
+print(minite)
+print(secend)
+
 
