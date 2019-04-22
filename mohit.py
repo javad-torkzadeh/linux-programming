95d530fc15437bbfa396beaddd57518139368338
javad torkzadeh <javadtorkzadeh1995@gmail.com>
2019-04-22 14:33:05 -0700

    mohit

---
+++ mohit.py
@@ -0,0 +1,9 @@
+radius = input("enter radius")
+radius = float (radius)
+radius=radius*0.0833
+diamiter =  2*radius
+mohit=diamiter*3.1415
+masahat=radius*radius*3.1415
+print ("diamiter is:{:10.4f}".format (diamiter))
+print("mohit is: {:10.4f}".format (mohit))
+print ("masahat is:{:10.4f}".format (masahat))
