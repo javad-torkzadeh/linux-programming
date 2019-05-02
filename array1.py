89b5639ae7bdcc370e5bff4e9562e738e3bd9db1
javad torkzadeh <javadtorkzadeh1995@gmail.com>
2019-05-02 01:42:42 -0700

    array

---
+++ array1.py
@@ -0,0 +1,5 @@
+aadad = [1,2,3,4,5]
+number = []
+for adad in range (len(aadad)) :
+    number.append(aadad[adad] + 2)
+    print(number [adad])
---
+++ array2.py
@@ -0,0 +1,5 @@
+array = [2,4,65,5]
+counter = 0
+for number in range(len(array)):
+    counter = counter + array[number]
+print(counter)
